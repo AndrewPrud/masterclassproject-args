be496ed3fcf041c164e2e940de1c682259bfeea0
Jihong <jihongbae2nd@gmail.com>
2022-06-23 10:47:38 -0500

    Update - EditWaste

--- src/components/waste/waste-forms/UpdateWaste.js
+++ src/components/waste/waste-forms/UpdateWaste.js
@@ -2,50 +2,70 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import './UpdateWaste.css';
 
-// CLick Edit Button
+// CLick Edit Button will be passsed ->
 // Pop Up Modal
 // Same UI as AddWaste
 // Input Click Done
-// Store to new Array
+// Store to new List
+// Change Previous Data
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+// delete button
 
-export default function BasicModal() {
+function UpdateWaste(props) {
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  // const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */} // get from
+      aniruddh by props
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Modaling...
-          </Typography>
+        <Box className="style">
+          <Grid container spacing={2}>
+            <Grid xs={12}>
+              <TextField label="Name" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="Owner" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="Price" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="City" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="State" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="Postal Code" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="Date Accepted" />
+            </Grid>
+            <Grid xs={12}>
+              <TextField label="Date Returned" />
+            </Grid>
+          </Grid>
         </Box>
       </Modal>
+      <footer>
+        <Button onClose={handleClose}>Close</Button>
+      </footer>
+      // store the new data here or send right away to aniruddh
     </div>
   );
 }
+
+export default UpdateWaste;
