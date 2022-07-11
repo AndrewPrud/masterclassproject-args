0c304bef7f96e6e0adb1881fc96ca34e1e09d34b
Aniket Singh <asingh11771@gmail.com>
2022-07-11 10:57:05 -0500

    Working Input

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -1,21 +1,61 @@
-import React from 'react';
-
-import Box, {
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
   TextField,
   FormGroup,
   FormControlLabel,
   Checkbox,
+  Grid,
 } from '@mui/material';
 
-function LoginForm() {
+function LoginForm(props) {
+  const [enteredUsername, setUsername] = useState('');
+  const [enteredPassword, setPassword] = useState('');
+
+  const userHandler = (event) => {
+    event.preventDefault();
+    const user = {
+      username: enteredUsername,
+      password: enteredPassword,
+    };
+    console.log(user);
+    setUsername('');
+    setPassword('');
+  };
   return (
-    <Box>
-      <TextField label="Username" />
-      <TextField label="Password" />
-      <FormGroup>
-        <FormControlLabel control={<Checkbox />} label="Remember Me" />
-      </FormGroup>
-    </Box>
+    <Container>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={6}>
+          <TextField
+            label="Username"
+            value={enteredUsername}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            label="Password"
+            value={enteredPassword}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Remember Me" />
+          </FormGroup>
+        </Grid>
+        <Grid item xs={6}>
+          <Button onClick={userHandler}>Submit</Button>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
