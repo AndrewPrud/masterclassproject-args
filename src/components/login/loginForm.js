335895219cb4504dac98568eb2d98c9da5f6298e
Aniket Singh <asingh11771@gmail.com>
2022-07-12 12:44:11 -0500

    Form Styling

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -2,87 +2,117 @@ import React, { useState } from 'react';
 import {
   Button,
   Container,
-  TextField,
   FormGroup,
   FormControlLabel,
   Checkbox,
   Grid,
   Typography,
   Box,
-  createTheme,
+  FormControl,
+  OutlinedInput,
 } from '@mui/material';
-import LoginButton from './loginButton';
 
 function LoginForm(props) {
   const [enteredUsername, setUsername] = useState('');
   const [enteredPassword, setPassword] = useState('');
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#004cbb',
-      },
-      secondary: {
-        main: '#ff7c00',
-      },
-    },
-  });
+
   const userHandler = (event) => {
     event.preventDefault();
     const user = {
       username: enteredUsername,
       password: enteredPassword,
     };
-    console.log(user);
     setUsername('');
     setPassword('');
+    console.log(user);
   };
 
-  // const submitDisabled = !enteredUsername || !enteredPassword;
+  const submitDisabled = !enteredUsername || !enteredPassword;
   return (
     <Container>
       <Box
         sx={{
           width: '100%',
-          height: 100,
-          backgroundColor: 'primary.main',
+          height: 125,
+          backgroundColor: '#004cbb',
+          flexGrow: 1,
           textAlign: 'center',
-          mb: 5,
+          mb: 7,
         }}
       >
-        <Typography variant="h4" fontWeight="bold">
-          LOG IN
+        <Typography variant="h4" fontWeight="bold" color="common.white">
+          {' '}
+          <br /> LOG IN
         </Typography>{' '}
       </Box>
-      <Grid
-        container
-        spacing={2}
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
+      <Box
+        sx={{
+          width: 270,
+          height: 370,
+          textAlign: 'center',
+          backgroundColor: '#f5f5f5',
+          alignItems: 'center',
+          ml: '38%',
+          mb: '24%',
+          borderRadius: 2,
+          boxShadow: 3,
+        }}
       >
-        <Grid item xs={6}>
-          <TextField
-            label="Username"
-            value={enteredUsername}
-            onChange={(event) => setUsername(event.target.value)}
-          />
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={6}>
+            <Typography variant="body1" fontWeight="bold" textAlign="left">
+              Username
+            </Typography>
+            <FormControl
+              sx={{ boxShadow: 2, bgcolor: 'common.white', mt: 1, mb: 2 }}
+            >
+              <OutlinedInput
+                value={enteredUsername}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" fontWeight="bold" textAlign="left">
+              Password
+            </Typography>
+            <FormControl sx={{ boxShadow: 2, bgcolor: 'common.white', mt: 1 }}>
+              <OutlinedInput
+                value={enteredPassword}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={6}>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox />}
+                label={
+                  <Typography
+                    variant="body1"
+                    fontWeight="bold"
+                    textAlign="left"
+                  >
+                    Remember Me
+                  </Typography>
+                }
+              />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={6}>
+            <Button disabled={submitDisabled} onClick={userHandler}>
+              {' '}
+              LOG IN{' '}
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label="Password"
-            value={enteredPassword}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="Remember Me" />
-          </FormGroup>
-        </Grid>
-        <Grid item xs={6}>
-          <LoginButton username={enteredUsername} password={enteredPassword} />
-        </Grid>
-      </Grid>
+      </Box>
     </Container>
   );
 }
