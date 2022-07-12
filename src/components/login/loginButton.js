d6ef8ab0253ce07a2a5d5d30a1c3b4f04c7df744
Jihong <jihongbae2nd@gmail.com>
2022-07-12 14:35:08 -0500

    commit message

--- src/components/login/loginButton.js
+++ src/components/login/loginButton.js
@@ -1,36 +1,20 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Modal from './errorModelTest';
+import React from 'react';
+import { Button } from '@mui/material';
 
-function LoginButton(props) {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+function loginButton(props) {
   const submitHandler = (event) => {
     event.preventDefault();
-
-    if (props.username && props.password) {
-      console.log(props.username);
-      console.log(props.password);
-    } else {
-      handleOpen();
-    }
+    console.log(props.username);
+    console.log(props.password);
   };
+
   return (
     <div>
       <Button variant="contained" onSubmit={submitHandler}>
         Login
       </Button>
-      <Modal
-        handleOpener={{ handleOpen }}
-        handleCloser={{ handleClose }}
-        isOpen={{ open }}
-      />
     </div>
   );
 }
 
-export default LoginButton;
+export default loginButton;
--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -10,12 +10,33 @@ import {
   Typography,
   Box,
   createTheme,
+  Modal,
+  Fade,
+  Backdrop,
 } from '@mui/material';
-import LoginButton from './loginButton';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function LoginForm(props) {
   const [enteredUsername, setUsername] = useState('');
   const [enteredPassword, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const theme = createTheme({
     palette: {
       primary: {
@@ -26,15 +47,31 @@ function LoginForm(props) {
       },
     },
   });
-  const userHandler = (event) => {
+  const handleLogin = (event) => {
     event.preventDefault();
-    const user = {
-      username: enteredUsername,
-      password: enteredPassword,
-    };
-    console.log(user);
-    setUsername('');
-    setPassword('');
+
+    setUsernameError(false);
+    setPasswordError(false);
+
+    if (enteredUsername === '') {
+      setUsernameError(true);
+    }
+
+    if (enteredPassword === '') {
+      setPasswordError(true);
+    }
+
+    if (enteredUsername && enteredPassword) {
+      const user = {
+        username: enteredUsername,
+        password: enteredPassword,
+      };
+      console.log(user);
+      setUsername('');
+      setPassword('');
+    } else {
+      handleOpen();
+    }
   };
 
   // const submitDisabled = !enteredUsername || !enteredPassword;
@@ -60,28 +97,60 @@ function LoginForm(props) {
         justifyContent="center"
         alignItems="center"
       >
-        <Grid item xs={6}>
-          <TextField
-            label="Username"
-            value={enteredUsername}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            label="Password"
-            value={enteredPassword}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <FormGroup>
-            <FormControlLabel control={<Checkbox />} label="Remember Me" />
-          </FormGroup>
-        </Grid>
-        <Grid item xs={6}>
-          <LoginButton username={enteredUsername} password={enteredPassword} />
-        </Grid>
+        <form noValidate autoComplete="off" onSubmit={handleLogin}>
+          <Grid item xs={6}>
+            <TextField
+              label="Username"
+              value={enteredUsername}
+              error={usernameError}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Password"
+              value={enteredPassword}
+              error={passwordError}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <FormGroup>
+              <FormControlLabel control={<Checkbox />} label="Remember Me" />
+            </FormGroup>
+          </Grid>
+          <Grid item xs={6}>
+            <Button type="submit" variant="contained" color="primary">
+              LOGIN
+            </Button>
+          </Grid>
+        </form>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <Box sx={style}>
+              <Typography
+                id="transition-modal-title"
+                variant="h6"
+                component="h2"
+              >
+                Bruh...😂
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                All Input Are Not Filled.
+              </Typography>
+            </Box>
+          </Fade>
+        </Modal>
       </Grid>
     </Container>
   );
